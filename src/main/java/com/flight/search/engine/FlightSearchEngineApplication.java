e41cfc31057bfa9551abe49267c435988ed8cf8d
Rafal Wisniewski <2krafal.wisniewski@gmail.com>
2022-10-26 20:30:56 +0200

    add info

--- src/main/java/com/flight/search/engine/FlightSearchEngineApplication.java
+++ src/main/java/com/flight/search/engine/FlightSearchEngineApplication.java
@@ -9,9 +9,9 @@ public class FlightSearchEngineApplication {
     public static void main(String[] args) {
         SpringApplication.run(FlightSearchEngineApplication.class, args);
     }
-    //TODO add info priority
     //TODO add luggage
     //TODO add clickable rows on admin page priority
     //TODO add tests priority
-    //TODO add meta-info priority
+    //TODO pretty page!
+    //TODO responsive page!
 }
