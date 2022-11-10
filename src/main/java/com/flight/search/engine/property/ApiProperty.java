2f83f3096e474d33bccc4d9e71d6d9cea8b4f920
Rafal Wisniewski <2krafal.wisniewski@gmail.com>
2022-11-10 13:50:50 +0100

    add api url to application.properties

---
+++ src/main/java/com/flight/search/engine/property/ApiProperty.java
@@ -0,0 +1,27 @@
+package com.flight.search.engine.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@EnableConfigurationProperties
+@Configuration
+@ConfigurationProperties(prefix="api")
+public class ApiProperty {
+    private String url;
+
+    public ApiProperty() {
+    }
+
+    public ApiProperty(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
